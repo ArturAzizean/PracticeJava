5f6e4dd621adceb788c88ad298bd7015139ba40c
Artur <arti996@yandex.ru>
2019-07-17 11:51:57 +0300

    some

--- projectBi/GradleApi/src/main/java/com/springGradle/GradleApi/repo/MessageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.springGradle.GradleApi.repo;
-
-import com.springGradle.GradleApi.domain.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepo extends JpaRepository<Message, Long> {
-
-}
--- projectBi/GradleApi/src/main/java/com/springGradle/GradleApi/repo/UserDetailsRepo.java
+++
@@ -1,7 +0,0 @@
-package com.springGradle.GradleApi.repo;
-
-import com.springGradle.GradleApi.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsRepo extends JpaRepository<User, String> {
-}
